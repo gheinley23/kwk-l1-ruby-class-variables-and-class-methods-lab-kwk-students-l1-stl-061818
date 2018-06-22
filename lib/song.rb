7060b33e8ab77b73b02dfe9bc7f61f98fea005c3
gheinley23 <gheinley23@jacksonindians.us>
2018-06-22 19:42:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
 class Song
   
+attr_accessor :jenje, :artist, :name  
+  
   def initialize(jenje,artist,name)
     @jenje = jenje
     @artist = artist
@@ -7,26 +9,26 @@ class Song
   end
 
 
-def gets_my_jenje
-@jenje 
-end
+# def gets_my_jenje
+# @jenje 
+# end
 
-def gets_artist
-  @artist
-end
+# def gets_artist
+#   @artist
+# end
 
-def gets_name
-@name
-end 
+# def gets_name
+# @name
+# end 
 
 
 end
 
 jon_song = Song.new("hippity hoppity", "jon mendez", "jon's way")
 
-puts jon_song.gets_my_jenje
+puts jon_song.jenje
 puts "wow! what a great choice!"
-puts jon_song.gets_name
-puts jon_song.gets_artist
+puts jon_song.name
+puts jon_song.artist
 
   
