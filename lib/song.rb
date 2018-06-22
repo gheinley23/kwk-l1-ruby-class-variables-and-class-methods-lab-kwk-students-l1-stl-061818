1b645c34a078be49caa4121e656b583029909840
gheinley23 <gheinley23@jacksonindians.us>
2018-06-22 19:32:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,29 @@
+class Song
+  
+  def initialize(jenje,artist,name)
+    @jenje = jenje
+    @artist = artist
+    @name = name 
+  end
 
+
+def gets_my_jenje
+@jenje 
+end
+
+def gets_artist
+  @artist
+end
+
+def gets_name
+@name
+end 
+
+
+end
+
+jon_song = Song.new("hippity hoppity", "jon mendez", "jon's way")
+
+
+end
+  
